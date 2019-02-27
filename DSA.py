e4c3f464361880d2ca9ddcb4bc8063961a9723bb
azax25547 <momentumangular@gmail.com>
2019-02-27 20:26:19 +0530

    updating excersice

--- DSA.py
+++ DSA.py
@@ -2,20 +2,35 @@
 # Binary Search
 
 
-def binary_serach(l, n):
-    # first lets brake the list and go to middle
-    length = len(l)
-    ll = None
-    arr = []
-    if length % 2 == 0:
-        ll = (length // 2) - 1
-    else:
-        ll = length // 2
-    # print(ll)
-    if n > l[ll]:
-        for i in range(ll+1, length):
-            arr.append(l[i])
-    (binary_serach(arr, n))
+# def binary_serach(l, n):
+#     # first lets brake the list and go to middle
+#     first = 0
+#     last = len(l) - 1
+#     while first <= last:
+#         mp = (first + last) / 2
+#         if (l[mp] == n):
+#             return mp
+#         elif (l[mp] < n):
+#             first = mp + 1
+#         else:
+#             last = mp - 1
+#     return -1
 
 
-binary_serach([1, 2, 3, 4, 5], 5)
+# binary_serach([1, 2, 3, 4, 5], 4)
+
+def bubble_sort(l):
+    n = len(l)
+    while 1:
+        swapped = False
+        for i in range(1, n):
+            if l[i-1] > l[i]:
+                (l[i-1], l[i]) = (l[i], l[i-1])
+                swapped = True
+        n = n-1
+        if not swapped:
+            break
+    return l
+
+
+print(bubble_sort([7,5,1,9,0]))
--- week3_practice.py
+++ week3_practice.py
@@ -10,12 +10,50 @@ def find_val_in_list(l, v):
 
 def descend(l):
     last = len(l) - 1
-    for i in range(last, -1, -1):
-        # print(l[i])
-        if l[i] < l[i-1] or l[i] == l[i-1]:
-            return(True)
-        else:
-            return(False)
+    procedd = False
+    if l == []:
+        return True
+    else:
+        for i in range(last, 0, -1):
+            # print(l[i])
+            if l[i] < l[i-1] or l[i] == l[i-1]:
+                procedd = True
+            else:
+                return(False)
+    if procedd:
+        return True
 
 
-print(descend([19, 17, 18, 7]))
+# print(descend([5,5,4,4,3,2,1]))
+
+def valley(l):
+    length = len(l)
+    begin = 0
+    if length <= 4:
+        return False
+    first = l[0]
+    last = l[-1]
+    mid = begin + length // 2
+    # return l[mid]
+    if first == last and first - l[mid] == last - l[mid] >= 2:
+        return True
+    else:
+        return False
+
+# print(valley([3,3,2,1,2]))
+
+
+def transpose(l):
+    length = len(l)
+    arr = []
+    if length == 1:
+        for item in l:
+            for it in item:
+                arr.append([it])
+        print(arr)                
+    elif length == 2:
+        l[0][1],l[1][1] = l[1][1],l[0][1]
+        print(l)
+    
+        
+transpose([[1,2,3],[3,4,5]])
